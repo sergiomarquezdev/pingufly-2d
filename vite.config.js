83af94461d74032f9576a802430cee63be932ba4
Sergio Márquez <44240815+sergiomarquezdev@users.noreply.github.com>
2025-06-19 00:42:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    open: true
-  },
+  base: '/pingufly-2d/',
+  server: { host: '0.0.0.0', port: 3000, open: true },
   build: {
     sourcemap: true,
-    assetsInlineLimit: 0, // No inlinear assets como base64
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          phaser: ['phaser']
-        }
-      }
-    }
+    assetsInlineLimit: 0,
+    rollupOptions: { output: { manualChunks: { phaser: ['phaser'] } } }
   },
-  optimizeDeps: {
-    exclude: ['fsevents']
-  },
-  publicDir: 'public',
-  base: './'
+  optimizeDeps: { exclude: ['fsevents'] },
+  publicDir: 'public'
 });
